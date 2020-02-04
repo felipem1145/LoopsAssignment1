3fd307ae371d0efef104d36ae0aac5e87ca48a87
junad ahmed <junad.ahmed21@outlook.com>
2020-02-03 19:39:10 -0500

    LA

---
+++ LoopsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte un numero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int i;
+            for (i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine(n + " x " + i+  " = " + (n * i));
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
